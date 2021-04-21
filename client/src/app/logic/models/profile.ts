bb21c85c73a6fb5e56167f68fba2c98218919ae4
IdanGib <idangibly.dev@gmail.com>
2021-04-21 09:37:27 +0300

    delete

--- client/src/app/logic/models/profile.ts
+++
@@ -1,8 +0,0 @@
-import { IProfile } from "../interfaces";
-import { AppUtils } from "../../logic/utils";
-export class Profile implements IProfile {
-    readonly id: string;
-    constructor(public name: string, public image: string) {
-        this.id = AppUtils.gId();
-    }
-}
--- client/src/app/logic/utils.ts
+++
@@ -1,6 +0,0 @@
-import { nanoid } from "nanoid";
-export class AppUtils {
-    static gId(): string {
-        return nanoid();
-    }
-}
